3e7d1c10b6264e6ad13279e29b3d3f6ec0d27d38
Thomas Löffler <loeffler@spooner-web.de>
2020-08-26 15:40:18 +0200

    Use shorter variant to install composer

--- .Build/docker_install.sh
+++ .Build/docker_install.sh
@@ -15,7 +15,4 @@ pecl install xdebug
 docker-php-ext-enable xdebug
 
 # Install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '572cb359b56ad9ae52f9c23d29d4b19a040af10d6635642e646a7caa7b96de717ce683bd797a92ce99e5929cc51e7d5f') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --filename=composer --install-dir=/usr/local/bin
-php -r "unlink('composer-setup.php');"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
