171d7d70e7a700779bae6ce7fd7ea30045ef72b7
Thomas Löffler <loeffler@spooner-web.de>
2021-01-28 17:17:20 +0100

    Add support for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0 - 10.4.99'
+            'typo3' => '8.7.0 - 11.9.99'
         ]
     ]
 ];
