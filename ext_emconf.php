10fab70a4483afb99654aebc5e9d4cccebb6df4e
Thomas Löffler <loeffler@spooner-web.de>
2024-04-11 23:47:11 +0200

    [TASK] Add version and compat to TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Spooner Web',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0 - 12.4.99',
+            'typo3' => '11.5.0 - 13.9.99',
         ],
     ],
 ];
